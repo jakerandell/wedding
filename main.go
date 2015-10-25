1ac22d798f2d338211336192b7f8740a4eb09a42
jakerandell <jakerandellphoto@gmail.com>
2015-10-25 00:08:53 -0400

    `date +"%Y-%m-%d @ %r"`

--- main.go
+++ main.go
@@ -16,11 +16,13 @@ func main() {
 
 	router := gin.New()
 	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
+//	router.LoadHTMLGlob("templates/*.tmpl.html")
+//	router.Static("/static", "static")
+
 
 	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+		name := c.Param("name")
+		c.String(http.StatusOK, "Hello %s", name)
 	})
 
 	router.Run(":" + port)
