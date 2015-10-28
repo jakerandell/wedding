4ba1fa74fd7bec3efe78a3ff6916a90f6e79604b
jakesym <jrand@symbolicinc.com>
2015-10-28 08:49:43 -0400

    switch to mux

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
-	"jakerandell.com/wedding/Godeps/_workspace/src/github.com/gin-gonic/gin"
+	//	"jakerandell.com/wedding/Godeps/_workspace/src/github.com/gin-gonic/gin"
+	"html/template"
+	"jakerandell.com/intense-forest-5272/Godeps/_workspace/src/github.com/gorilla/mux"
+	"log"
 	"net/http"
 	"os"
 )
@@ -14,24 +17,39 @@ func main() {
 		port = "5000"
 	}
 
-	router := gin.New()
-	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.html")
-	router.Static("/audio", "audio")
-	router.Static("/css", "css")
-	router.Static("/images", "images")
-	router.Static("/js", "js")
-	router.Static("/video", "video")
+	//	router := gin.New()
+	//	router.Use(gin.Logger())
+	//	router.LoadHTMLGlob("templates/*.html")
+	//	router.Static("/audio", "audio")
+	//	router.Static("/css", "css")
+	//	router.Static("/images", "images")
+	//	router.Static("/js", "js")
+	//	router.Static("/video", "video")
 
+	/*router.GET("/", func(c *gin.Context) {
+	//		c.String(http.StatusOK, "We're getting married. Give us money.")
+			c.HTML(http.StatusOK, "index.html", gin.H{})
+		})*/
 
-	router.GET("/", func(c *gin.Context) {
-//		c.String(http.StatusOK, "We're getting married. Give us money.")
-		c.HTML(http.StatusOK, "index.html", gin.H{})
-	})
-
-	router.GET("/loaderio-21e121865e59f3867a444fbdb50f665d/", func(c *gin.Context) {
+	/*router.GET("/loaderio-21e121865e59f3867a444fbdb50f665d/", func(c *gin.Context) {
 		c.String(http.StatusOK, "loaderio-21e121865e59f3867a444fbdb50f665d")
+	})*/
+
+	//	router.Run(":" + port)
+
+	r := mux.NewRouter()
+	//	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	r.PathPrefix("/images/").Handler(http.StripPrefix("/images/", http.FileServer(http.Dir("./images/"))))
+	r.PathPrefix("/css/").Handler(http.StripPrefix("/css/", http.FileServer(http.Dir("./css/"))))
+	r.PathPrefix("/js/").Handler(http.StripPrefix("/js/", http.FileServer(http.Dir("./js/"))))
+
+	tmpl := template.Must(template.ParseGlob("templates/*"))
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl.ExecuteTemplate(w, "index.html", nil)
 	})
 
-	router.Run(":" + port)
+	//	r.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":"+port, r))
 }
